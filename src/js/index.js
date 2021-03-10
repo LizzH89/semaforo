78694a928a15c8b2bbe4cdfc8a27e94f4370afca
LizzH89 <lizziehidalgoleiva@gmail.com>
2021-03-10 23:01:59 +0000

    Semaforo

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClock } from "@fortawesome/free-solid-svg-icons";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
@@ -20,45 +20,44 @@ import PropTypes from "prop-types";
 
 ///////////////////////wraping//////////////////////////////////////////////////
 
-function Contador(props) {
-	return (
-		<div className="primero">
-			<div className="reloj">
-				<i className="fas fa-clock"></i>
+class Lizzie extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			theOne: null
+		};
+	}
+
+	render() {
+		console.log(this.state);
+		let A;
+		let B;
+		let C;
+		if (this.state.theOne == "Rojo") A = "selected"; //esto para poder concatenar y que si se le hace click quede como "Rojo selected"
+		if (this.state.theOne == "Naranja") B = "selected";
+		if (this.state.theOne == "Verde") C = "selected";
+
+		return (
+			<div className="Todo">
+				<div
+					className={"Rojo " + A}
+					onClick={() => this.setState({ theOne: "Rojo" })}>
+					<i className="fas fa-circle"></i>
+				</div>
+				<div
+					className={"Naranja " + B}
+					onClick={() => this.setState({ theOne: "Naranja" })}>
+					<i className="fas fa-circle"></i>
+				</div>
+				<div
+					className={"Verde " + C}
+					onClick={() => this.setState({ theOne: "Verde" })}>
+					<i className="fas fa-circle"></i>
+				</div>
 			</div>
-			<div className="byte4">{props.tCuatro}</div>
-			<div className="byte3">{props.tTres}</div>
-			<div className="byte2">{props.tDos}</div>
-			<div className="byte1">{props.tUno}</div>
-			<div className="byte0">{props.tZero}</div>
-		</div>
-	); //este es el del main
+		);
+	}
 }
 
-Contador.propTypes = {
-	tCuatro: PropTypes.number,
-	tTres: PropTypes.number,
-	tDos: PropTypes.number,
-	tUno: PropTypes.number,
-	tZero: PropTypes.number
-};
-
-let counter = 0; //Counter va a ser la variable que le vamos a aumentar ++ cada vez
-setInterval(function() {
-	let cuatro = Math.floor(counter / 10000);
-	let tres = Math.floor(counter / 1000);
-	let dos = Math.floor(counter / 100);
-	let uno = Math.floor(counter / 10);
-	let zero = Math.floor(counter / 1);
-	counter++;
-	ReactDOM.render(
-		<Contador
-			tZero={zero % 10}
-			tUno={uno % 10}
-			tDos={dos % 10}
-			tTres={tres % 10}
-			tCuatro={cuatro % 10}
-		/>,
-		document.querySelector("#app")
-	);
-}, 1000);
+//render your react application
+ReactDOM.render(<Lizzie />, document.querySelector("#app"));
